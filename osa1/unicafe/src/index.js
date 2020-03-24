698fa8b864372d5fa9a16c8e8de3656a40b48a75
Pyry <pyry.v.seppala@aalto.fi>
2020-03-24 17:56:56 +0200

    unicafe tehty

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -10,6 +10,46 @@ const Button = (props) => {
   )
 }
 
+const average = (good, bad, neutral) => {
+  return(
+    ((bad*-1) + (good*1) + (neutral*0)) / (good+bad+neutral)
+  )
+}
+
+const positivePr = (good, bad, neutral) => {
+  return(
+    (good/(good+bad+neutral))*100 + ' %'
+  )
+}
+const StatisticLine = (props) => {
+  return(
+    <tr>
+    <th>{props.text}</th> <td>{props.value}</td>
+    </tr>
+  )
+}
+const Statistics = (props) => {
+  if(props.good===0 & props.bad===0 & props.neutral===0) {
+    return (
+      <div>
+        <h2>No feedback given</h2>
+      </div>
+    )
+  }
+  return(
+  <div>
+    <h2>Statistics</h2>
+    <table>
+    <StatisticLine text="good" value ={props.good}/>
+    <StatisticLine text="neutral" value ={props.neutral} />
+    <StatisticLine text="bad" value ={props.bad} />
+    <StatisticLine text="all" value ={props.good + props.bad + props.neutral} />
+    <StatisticLine text="average" value ={average(props.good, props.bad, props.neutral)} />
+    <StatisticLine text="positive" value={positivePr(props.good, props.bad, props.neutral)}  />
+    </table>
+  </div>
+  )
+}
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -25,12 +65,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
     </div>
-    <div>
-      <h2>Statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-    </div>
+    <Statistics good={good} bad={bad} neutral={neutral} />
     </>
   )
 }
