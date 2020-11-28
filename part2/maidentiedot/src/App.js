1f12d8c647a54de669195f2d8dd4bd1e2a6cddb5
Pyry Seppälä <pyry.v.seppala@gmail.com>
2020-11-28 15:36:00 +0200

    maiden tiedot tehty, api avaimessa häikkää?

---
+++ part2/maidentiedot/src/App.js
@@ -0,0 +1,126 @@
+
+import './App.css';
+import React, {useState, useEffect} from 'react'
+import Axios from 'axios';
+
+const Country = ({country}) => {
+  const [weather, setWeatherData] = useState([])
+  const apiKey = process.env.REACT_APP_API_KEY
+  console.log(apiKey)
+  console.log("apirequest: ", `http://api.weatherstack.com/current
+  ?access_key=${apiKey}
+  &query=${country.capital}`)
+
+  
+
+  useEffect(() => {
+    Axios.get(`http://api.weatherstack.com/current
+    ? access_key = ${apiKey}
+    & query = ${country.capital}`)
+    .then(response => {
+      console.log("weather data: " ,response)
+      setWeatherData(response.data)
+    }).catch(error => {
+      console.log(error)
+    })
+  },[apiKey,country.capital])
+
+  return(
+    <div>
+      <h2>{country.name}</h2>
+      <p>Capital {country.capital}</p>
+      <p>Population {country.population}</p>
+      <h4>Languages</h4>
+      {country.languages.map(l => <p key={l.iso639_2}>{l.name}</p>)}
+      <img src={country.flag} width="200px" height="150px" />
+      <div>
+        <h4>Weather in {country.capital} </h4>
+        {/* <p>{weather.current.weather_descriptions[0]}</p>
+        <p>temperature: {weather.current.temperature} C</p>
+        <img src={weather.current.weather_icons[0]} width="100px" height="100px" />
+        <p>Wind: {weather.current.wind_speed}</p> */}
+      </div>
+    </div>
+  )
+}
+const CountryName = ({country}) => {
+  const [showCountry, setShowCountry] = useState(false)
+  const handleClick = (event) => {
+    setShowCountry(!showCountry)
+  }
+  if(showCountry) {
+    return(
+      <>
+      <button onClick={handleClick}>Hide</button>
+      <Country country={country} />
+      </>
+    )
+  } else {
+  return(
+    
+  <div>
+    {country.name} <button onClick={handleClick}>Show</button>
+  </div>
+  )
+  }
+}
+const CountiresList = ({countries}) => {
+  if(countries.length === 1) {
+    console.log(countries[0])
+    return(
+      <Country country={countries[0]} />
+    )
+  } else if(countries.length <= 10) {
+    return(
+      <div>
+        {countries.map(c => <CountryName key={c.alpha3Code} country={c} />)}
+      </div>
+    )
+  } else {
+    return(
+      <div>
+        Too many matches, please specify more accurate filter
+      </div>
+    )
+  }
+}
+
+function App() {
+  const [countries, setCountries] = useState([])
+  const [searchTerm, setSearch] = useState('')
+
+  const hook = () => {
+    Axios.get('https://restcountries.eu/rest/v2/all')
+    .then(response => {
+      console.log(response)
+      setCountries(response.data)
+    }).catch(error => {
+      console.log("Error: ", error);
+    })
+  }
+
+  useEffect(hook, [])
+
+
+  
+
+  const handleInput = (event) => {
+    console.log(event.target.value)
+    setSearch(event.target.value)
+  }
+  const dSearch = () => {
+    console.log(countries.filter(c => c.name.toLowerCase().includes(searchTerm.toLowerCase())))
+    return countries.filter(c => c.name.toLowerCase().includes(searchTerm.toLowerCase()))
+  }
+
+  return (
+    <div className="App">
+    <div className="search">
+      find countries: <input value={searchTerm} onChange={handleInput}/>
+    </div>
+      <CountiresList countries={dSearch()} />
+    </div>
+  );
+}
+
+export default App;
