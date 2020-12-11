3b1fca678fc28118ecb77aee8a0bb62a977ad186
Pyry Seppälä <pyry.v.seppala@gmail.com>
2020-12-11 18:20:24 +0200

    blogilista step 1 & 2 done

---
+++ part4/4.1-blogilista/app.js
@@ -0,0 +1,28 @@
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const app = express()
+
+const mongoUrl = config.MONGODB_URI
+
+logger.info('connecting to ', mongoUrl)
+
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true 
+})
+.then(() => {
+    logger.info('connected succesfully to MongoDB')
+})
+.catch((error) => {
+    logger.error('error connecting to MongoDB', error.message)
+})
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
