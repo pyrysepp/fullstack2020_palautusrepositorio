b8fd4d2252c27c82bb3a570be9b2bb0789327c98
Pyry Seppälä <pyry.v.seppala@gmail.com>
2021-01-09 17:22:55 +0200

    random

---
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "../styles/Blog.css";
+import blogService from "../services/blogService";
+
+const Blog = ({ blog, removeBlog }) => {
+  const [showFull, setShowFull] = useState(false);
+  const [likes, setLikes] = useState(blog.likes);
+
+  const handleLike = async () => {
+    try {
+      setLikes(likes + 1);
+      await blogService.likeBlog({
+        ...blog,
+        likes: likes + 1,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  if (!showFull) {
+    return (
+      <div className="Blog">
+        {blog.title} {blog.author}
+        <button onClick={() => setShowFull(!showFull)}>view</button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Blog">
+        {blog.title}{" "}
+        <button onClick={() => setShowFull(!showFull)}>hide</button>
+        <p>{blog.url}</p>
+        <p>
+          {likes} <button onClick={handleLike}>like</button>
+        </p>
+        <p>{blog.author}</p>
+        <button onClick={() => removeBlog(blog.id)}>remove</button>
+      </div>
+    );
+  }
+};
+
+export default Blog;
