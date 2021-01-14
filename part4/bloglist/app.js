755c6c73951721f24fd788ae95771f6685c4adad
Pyry Seppälä <pyry.v.seppala@gmail.com>
2021-01-14 15:33:27 +0200

    test api working

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,11 +4,13 @@ const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
 const logger = require("./utils/logger");
 const config = require("./utils/config");
+require("dotenv").config();
 const tokenExtractor = require("./utils/tokenExtractor");
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const errorHandler = require("./utils/error_handler");
+
 
 const app = express();
 
@@ -29,13 +31,17 @@ mongoose
   .catch((error) => {
     logger.error("error connecting to MongoDB", error.message);
   });
-
+console.log(process.env.NODE_ENV)
 app.use(cors());
 app.use(express.json());
 app.use(tokenExtractor);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing")
+  app.use("/api/testing", testingRouter)
+}
 app.use(errorHandler);
 
 module.exports = app;
