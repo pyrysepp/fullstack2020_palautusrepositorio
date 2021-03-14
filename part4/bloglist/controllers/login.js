e73d2419d61d630102598154208c07af99129089
Pyry Seppälä <pyry.v.seppala@gmail.com>
2021-03-14 17:29:24 +0200

    error handling better

--- part4/bloglist/controllers/login.js
+++ part4/bloglist/controllers/login.js
@@ -1,36 +1,38 @@
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const loginRouter = require("express").Router();
-const User = require("../models/user");
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const loginRouter = require("express").Router()
+const User = require("../models/user")
 
 loginRouter.post("/", async (req, res, next) => {
-  const body = req.body;
+  const body = req.body
 
-  const user = await User.findOne({ username: body.username });
+  const user = await User.findOne({ username: body.username })
 
-  const passwordCorrect = async () => {
-    if (!user === null) {
-      await bcrypt.compare(body.password, user.passwordHash);
-    }
-  };
-  if (user === null || !passwordCorrect) {
-    console.log(user, passwordCorrect);
-    return res.status(401).json({
-      error: "invalid username or password",
-    });
+  const passwordCorrect =
+    user === null
+      ? false
+      : await bcrypt.compare(body.password, user.passwordHash)
+
+  if (!(user !== null && passwordCorrect)) {
+    console.log("moi")
+
+    return res.status(401).send({
+      message: "invalid username or password",
+    })
   }
+
   const userFortoken = {
     username: user.username,
     id: user._id,
-  };
+  }
 
-  const token = jwt.sign(userFortoken, process.env.SECRET);
-
+  const token = jwt.sign(userFortoken, process.env.SECRET)
+  console.log("we here")
   res.status(200).json({
     token,
     username: user.username,
     name: user.name,
-  });
-});
+  })
+})
 
-module.exports = loginRouter;
+module.exports = loginRouter
--- part4/bloglist/utils/error_handler.js
+++ part4/bloglist/utils/error_handler.js
@@ -1,20 +1,20 @@
-require("mongoose");
+require("mongoose")
 
 const errorHandler = (error, request, response, next) => {
-  console.log(error);
+  console.log("error handler: ", error)
 
   if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
+    return response.status(400).send({ message: "malformatted id" })
   }
   if (error.name === "ValidationError") {
-    return response.status(400).send({ error: error.message });
+    return response.status(400).send({ message: error.message })
   }
   if (error.name === "JsonWebTokenError") {
     return response.status(401).json({
       error: "invalid token",
-    });
+    })
   }
-  next(error);
-};
+  next(error)
+}
 
-module.exports = errorHandler;
+module.exports = errorHandler
